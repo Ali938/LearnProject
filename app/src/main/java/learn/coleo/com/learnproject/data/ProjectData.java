e54689d464d43a22cdf2ca6e2cb6bbeb59b8e08e
Alireza Rahimi <ali938414@gmail.com>
2019-02-28 11:56:59 +0330

    added "projectData" abstract java class for next use

---
+++ app/src/main/java/learn/coleo/com/learnproject/data/ProjectData.java
@@ -0,0 +1,14 @@
+package learn.coleo.com.learnproject.data;
+
+public abstract class ProjectData {
+
+    private int progress;
+
+    public void setProgress(int progress) {
+        this.progress = progress;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+}
